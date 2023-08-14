57755ba9d407a335e60be12b2abd71b38f1bac04
tapp41k <icepeak5@yandex.ru>
2023-08-15 00:54:54 +0300

    deploy1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -14,7 +14,7 @@ DEBUG = (os.getenv('DEBUG', 'False') == 'True')
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(' ')
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
 
