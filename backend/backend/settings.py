7177d9bf0735c5bc94a411badb4866b009e598fd
tapp41k <icepeak5@yandex.ru>
2023-08-15 00:11:56 +0300

    deploy1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -73,8 +73,8 @@ else:
         'default': {
             'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
             'NAME': os.getenv('DB_NAME', 'postgres'),
-            'USER': os.getenv('POSTGRES_USER', 'admin'),
-            'PASSWORD': os.getenv('POSTGRES_PASSWORD', '12345'),
+            'USER': os.getenv('POSTGRES_USER', 'postgres'),
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
             'HOST': os.getenv('DB_HOST', 'db'),
             'PORT': os.getenv('DB_PORT', '5432')
         }
