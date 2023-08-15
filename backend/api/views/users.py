20339bec4736a861acc177ba290b64c0fb553155
tapp41k <icepeak5@yandex.ru>
2023-08-15 18:28:12 +0300

    deploy3

--- backend/api/views/users.py
+++ backend/api/views/users.py
@@ -3,7 +3,6 @@ from djoser.views import UserViewSet
 from rest_framework import permissions, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework import permissions
 
 from api.serializers.users import (CustomUserSerializer,
                                    SubscriptionSerializer,
